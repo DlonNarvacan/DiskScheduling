a6098a74bedbd50624f3805395a92f9eef42edcd
Arnold Narvacan <bignetnarvacan@yahoo.com>
2021-06-08 02:46:27 +0800

    with chart included

--- fcfs.js
+++ fcfs.js
@@ -3,24 +3,51 @@ let reqs,
   st = 0;
 const request = [];
 const stchart = [];
+const new_stchart = [];
+const chart = [];
+
+console.log("===== FCFS Disk Scheduling Algorithm =====");
 
 reqs = prompt("Number of Requests: ");
 console.log(`Number of Request: ${reqs}`);
 
 head = prompt("Headstart: ");
-console.log(`Headstart ${head}`);
+console.log(`Headstart: ${head}`);
+stchart.push(head);
 
 for (let i = 0; i < reqs; i++) {
   request.push(`${prompt(`Queue ${i + 1} :`)}`);
   console.log(`Queue ${i + 1}: ${request[i]}`);
 }
 
-stchart.push(head);
 for (let i = 0; i < reqs; i++) {
   stchart.push(request[i]);
   st += Math.abs(request[i] - head);
   head = request[i];
 }
 
-console.log(stchart.join(" -> "));
+console.log("============= FCFS Seek Time Movement Chart =============");
+
+for (let i = 0; i < stchart.length; i++) new_stchart.push(stchart[i]);
+
+stchart.sort((a, b) => a - b);
+console.log(`=  ${stchart.join(" | ")} |`);
+
+let temp;
+for (let i = 0; i < new_stchart.length; i++) {
+  temp = new_stchart[i];
+  for (let j = 0; j < new_stchart.length; j++) {
+    if (temp == stchart[j]) {
+      if (stchart[j] <= 99) chart.push(" ■ |");
+      else chart.push(" ■  |");
+    } else {
+      if (stchart[j] <= 99) chart.push("   |");
+      else chart.push("    |");
+    }
+  }
+  console.log(`=  ${chart.join(" ")}`);
+  chart.splice(0, chart.length);
+}
+
+console.log(`Seek Movement: ${new_stchart.join(" - ")}`);
 console.log(`Total Seek Time: ${st}`);
--- sstf.js
+++ sstf.js
@@ -4,12 +4,17 @@ let reqs,
 const request = [];
 const stchart = [];
 const visited = [];
+const new_stchart = [];
+const chart = [];
+
+console.log("===== SSTF Disk Scheduling Algorithm =====");
 
 reqs = prompt("Number of Requests: ");
 console.log(`Number of Request: ${reqs}`);
 
 head = prompt("Headstart: ");
-console.log(`Headstart ${head}`);
+console.log(`Headstart: ${head}`);
+stchart.push(head);
 
 for (let i = 0; i < reqs; i++) {
   request.push(`${prompt(`Queue ${i + 1} :`)}`);
@@ -17,22 +22,44 @@ for (let i = 0; i < reqs; i++) {
   console.log(`Queue ${i + 1}: ${request[i]}`);
 }
 
-stchart.push(head);
 for (let i = 0; i < reqs; i++) {
   let temp = 1e9,
     k = -1;
-  for (let j = 0; j < reqs; j++) {
+  for (let j = 0; j < reqs; j++)
     if (head != request[j] && visited[j] == 0)
       if (temp > Math.abs(head - request[j])) {
         temp = Math.abs(head - request[j]);
         k = j;
       }
-  }
+
   st += temp;
   stchart.push(request[k]);
   head = request[k];
   visited[k] = 1;
 }
 
-console.log(stchart.join(" -> "));
+console.log("============= SSTF Seek Time Movement Chart =============");
+
+for (let i = 0; i < stchart.length; i++) new_stchart.push(stchart[i]);
+
+stchart.sort((a, b) => a - b);
+console.log(`=  ${stchart.join(" | ")} |`);
+
+let temp;
+for (let i = 0; i < new_stchart.length; i++) {
+  temp = new_stchart[i];
+  for (let j = 0; j < new_stchart.length; j++) {
+    if (temp == stchart[j]) {
+      if (stchart[j] <= 99) chart.push(" ■ |");
+      else chart.push(" ■  |");
+    } else {
+      if (stchart[j] <= 99) chart.push("   |");
+      else chart.push("    |");
+    }
+  }
+  console.log(`=  ${chart.join(" ")}`);
+  chart.splice(0, chart.length);
+}
+
+console.log(`Seek Movement: ${new_stchart.join(" - ")}`);
 console.log(`Total Seek Time: ${st}`);
